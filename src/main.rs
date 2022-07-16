ec3ebceaecfbdd0a58442b129626608c911e5580
jervw <vuolajere@gmail.com>
2022-07-16 11:47:13 +0300

    working prototype

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let response = reqwest::blocking::get("https://urbaanisanakirja.com/word/kehari")
+        .unwrap()
+        .text()
+        .unwrap();
+
+    let doc = scraper::Html::parse_document(&response);
+
+    let container = scraper::Selector::parse("div.box").unwrap();
+    let definition = scraper::Selector::parse("p").unwrap();
+
+    let entries = doc.select(&container);
+
+    for entry in entries {
+        entry
+            .select(&definition)
+            .for_each(|x| println!("{}", x.inner_html()));
+    }
 }
