e786e25cd4646926368b33920da1d59f143dc5a8
jervw <vuolajere@gmail.com>
2022-08-07 19:12:59 +0300

    add more metadata

--- src/lib.rs
+++ src/lib.rs
@@ -3,21 +3,43 @@ use scraper::{Html, Selector};
 use std::env;
 use std::error::Error;
 use std::process;
+use termion::{color, style};
 
 const URL: &str = "https://urbaanisanakirja.com";
 fn parse_data(data: &str) {
-    let doc = Html::parse_document(&data);
+    let body = Html::parse_document(&data);
 
     let container = Selector::parse("div.box").unwrap();
-    let definition = Selector::parse("p").unwrap();
 
-    let entries = doc.select(&container);
-    
+    let heading = Selector::parse("h1").unwrap();
+    let definition = Selector::parse("p").unwrap();
+    let user = Selector::parse("user").unwrap();
+    let date = Selector::parse("span.datetime").unwrap();
+
+    let entries = body.select(&container);
 
     for entry in entries {
-        entry
-            .select(&definition)
-            .for_each(|x| println!("{}", x.inner_html()));
+        //let heading = entry.select(&heading).next().unwrap();
+        //let definition = entry.select(&definition).next().unwrap();
+        //let date = entry.select(&date).next().unwrap();
+
+        //let user_string: String = user.text().collect();
+        //let date_string: String = date.text().collect();
+
+        //println!("{}", user_string);
+        let test: &str = "Tama on testisad askdakls lkas dl";
+        println!(
+            "{}{}{} {} \n{}",
+            style::Bold,
+            color::Bg(color::Cyan),
+            color::Fg(color::Black),
+            &test,
+            style::Reset
+        );
+
+        println!(" - {}{}", style::Italic, textwrap::fill(&test, 60));
+
+        println!("\n{}{}{}", style::Bold, &test, style::Reset);
     }
 }
 pub fn search_query(query: &str) -> Result<(), Box<dyn Error>> {
